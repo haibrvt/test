3fab0eac0783ed78ad21e9f17bd4decb0eb1ab0f
haibrvt <40351109+haibrvt@users.noreply.github.com>
2023-02-13 10:28:41 +0700

    Update test.js

--- test.js
+++ test.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://ipv4.webshare.io/*
+// @updateURL    https://raw.githubusercontent.com/haibrvt/test/main/test.js
 // @downloadURL  https://raw.githubusercontent.com/haibrvt/test/main/test.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
