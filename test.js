e34764554e59cabaeb67c250beb608b536f63af5
haibrvt <40351109+haibrvt@users.noreply.github.com>
2023-02-13 10:07:14 +0700

    Update test.js

--- test.js
+++ test.js
@@ -6,6 +6,7 @@
 // @author       You
 // @match        https://ipv4.webshare.io/*
 // @updateURL    https://github.com/mygithubaccount/test/raw/master/test.user.js
+// @downloadURL  https://github.com/mygithubaccount/test/raw/master/test.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
