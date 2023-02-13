ea9e6bc14c0d188fef956c77d896ba1d12f39889
haibrvt <40351109+haibrvt@users.noreply.github.com>
2023-02-13 10:19:21 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.11
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://ipv4.webshare.io/*
